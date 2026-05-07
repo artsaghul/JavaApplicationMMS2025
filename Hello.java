a7ddfcdd66fa051409971c6c32e26fc8401c6d2a
artsaghul <bekeoweibrighton@gmail.com>
2026-05-07 10:53:01 +0100

    Assignment 1

---
+++ Hello.java
@@ -0,0 +1,14 @@
+public class Hello{
+	public static void main(String[] args){ 
+	   System.out.printin("Hello everyone, I am learning Java");
+	   System.out.print("Oh wow, Learning Java is fun. ");
+	   System.out.print("Hurray, I am Happy. ");
+	   System.out.printf("Java was created in %d",1992);
+	   System.out.printf("Java was made free for %.2f dollar",0.0000);
+	   System.out.printf("Java was made free for %c%.2f",'$' ,0.0000);
+	   System.out.printf("Do you love Java%c %b", '?' ,true);
+	   System,out.printf("%s",Yes learning %s is fun","Java");
+	   
+	}
+	
+}	
