6712d1e01f8fe97fc05c3ecd7a34e52746258050
artsaghul <bekeoweibrighton@gmail.com>
2026-05-11 10:43:21 +0100

    apello

---
+++ new 1.js
@@ -0,0 +1,12 @@
+public class EscapeCharacter(
+     public static void main(String[] args){
+		 System.out.print("When you need to use the escape characters \n ");
+		 Syste.out.print("Use it when you want to manupulate your cursor");
+		 System.out.print("Use \\n when you want a newhile");
+		 System.out.print("Who is the father of java?. The father of java is"James Goosling\"");
+		 System.out.print("\Use this when you want a tab or indentation");
+		 System.out.print("We are using carriage return here");
+	}
+		 
+		 
+}		 		 
---
+++ new 3.js
@@ -0,0 +1,29 @@
+public class PrimitiveDataTypes{
+	public static void main(String[] args){
+		byte Age = 30;
+		System.out.printf("Miss mercy is %d years old",myage);
+		
+		short quantity=2000;
+		System.out.print("The quanatity of phone ordered is %,d%n",quantity);
+		
+		int nigeriaPopulation = 29484848;
+		System.out.printf("The population of nigeria is %,d%n<nigeriapopulation");
+		
+		long worldPopulation = 4747474847874874;
+		System.out.printf("The world's population is %,d%n",worldPopulation);
+		
+		float price = 57876.584747:
+		System.out.printin("The price of each iphone per unit is %,.2f%n",price);
+		
+		double myBalance = 7465565655767.764;
+		System.out.printf("My account balance is %c%,.2f%n", '$' ,myBalance);
+		
+		char symbol = '%';
+		
+		System.out.printf("There is an increment in the world's population by 20.5%c%n",symbol);
+		
+		boolean isJavaFun = true;
+		System.out.printf("Do you love Java? %b" ,isJavaFun");
+		{
+{ 
+					
